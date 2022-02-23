272cd3f5b7eb07dc77869d395b9b12bd2bc3b4ec
ritwikdiv <ritwik.edu@gmail.com>
2022-02-22 16:15:32 -0800

    refactor: moving theme to styles

---
+++ styles/theme.js
@@ -0,0 +1,9 @@
+import '@fontsource/dm-sans';
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	fonts: {
+		heading: 'DM Sans, sans-serif',
+		body: 'DM Sans, sans-serif',
+	},
+});
