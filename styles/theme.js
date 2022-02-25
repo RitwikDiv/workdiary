6882934b3866a0b404a02b5d6784b68c5fa7f487
ritwikdiv <ritwik.edu@gmail.com>
2022-02-24 22:02:25 -0800

    feature: modified overall theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,27 @@
 import '@fontsource/dm-sans';
 import { extendTheme } from '@chakra-ui/react';
+import Button from './components/button';
+import Text from './components/text';
+import Link from './components/link';
 
 export const theme = extendTheme({
 	fonts: {
 		heading: 'DM Sans, sans-serif',
 		body: 'DM Sans, sans-serif',
 	},
+	semanticTokens: {
+		colors: {
+			primary: 'purple.500',
+			secondary: 'gray.700',
+			textPrimary: {
+				default: 'gray.700',
+				_dark: 'gray.200',
+			},
+			textSecondary: {
+				default: 'gray.600',
+				_dark: 'gray.400',
+			},
+		},
+	},
+	components: { Button, Text, Link },
 });
