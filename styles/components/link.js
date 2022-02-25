965a45c8bb9ee11ca8319160f98ffdb36f850b3c
ritwikdiv <ritwik.edu@gmail.com>
2022-02-24 22:02:00 -0800

    feature: added link theme

---
+++ styles/components/link.js
@@ -0,0 +1,24 @@
+const Link = {
+	baseStyle: {
+		color: 'textPrimary',
+		fontSize: 'md',
+		fontWeight: 'bold',
+		textDecoration: 'underline',
+		_hover: {
+			color: 'primary',
+		},
+	},
+	sizes: {
+		sm: {
+			fontSize: 'sm',
+		},
+		md: {
+			fontSize: 'md',
+		},
+	},
+	defaultProps: {
+		size: 'md',
+	},
+};
+
+export default Link;
