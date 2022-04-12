2c9e1bfc81b41b65c2e0df02c5ac95f1ad71578c
ritwikdiv <ritwik.edu@gmail.com>
2022-04-12 09:17:19 -0700

    feature: created a footer for the app

--- components/footers/SimpleFooter.js
+++ components/footers/SimpleFooter.js
@@ -1,20 +1,7 @@
-import {
-	Button,
-	HStack,
-	IconButton,
-	Spacer,
-	Container,
-} from '@chakra-ui/react';
+import { IconButton, Spacer, Container, Stack, Link } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import {
-	BsDiscord as Discord,
-	BsLinkedin as LinkedIn,
-	BsListCheck as Terms,
-} from 'react-icons/bs';
-import {
-	MdOutlineCelebration as Started,
-	MdOutlinePrivacyTip as Privacy,
-} from 'react-icons/md';
+import { BsDiscord as Discord, BsLinkedin as LinkedIn } from 'react-icons/bs';
+import NextLink from 'next/link';
 
 export default function SimpleFooter() {
 	const linkList = [
@@ -30,47 +17,55 @@ export default function SimpleFooter() {
 		},
 	];
 	const pagesList = [
-		{ name: 'Get Started', path: '/login', icon: <Started></Started> },
-		{ name: 'Privacy', path: '/privacy', icon: <Privacy></Privacy> },
-		{ name: 'Terms', path: '/terms', icon: <Terms></Terms> },
+		{
+			name: 'Home',
+			path: '/',
+		},
+		{
+			name: 'Privacy Policy',
+			path: '/privacy',
+		},
+		{ name: 'Terms & Conditions', path: '/terms' },
+		{
+			name: 'Get Started',
+			path: '/login',
+		},
 	];
 	const router = useRouter();
+
 	return (
-		<Container as='footer' maxWidth={'container.lg'}>
-			<HStack
-				flexWrap={'wrap'}
-				width={'100%'}
-				align='center'
-				bottom={0}
-				spacing={2}
-				paddingTop={5}
-				paddingBottom={5}>
-				{pagesList.map((pageItem) => {
-					return (
-						<Button
-							variant='menu'
-							key={pageItem.name}
-							onClick={() => router.push(pageItem.path)}
-							leftIcon={pageItem.icon}>
-							{pageItem.name}
-						</Button>
-					);
-				})}
+		<Container
+			maxWidth={'container.xl'}
+			marginTop={5}
+			bottom={0}
+			marginBottom={5}>
+			<Stack flexWrap={'wrap'} align='center' direction={'row'}>
+				<Stack direction={'row'} spacing={3} align='center'>
+					{pagesList.map((pageItem) => {
+						return (
+							<NextLink href={pageItem.path} passHref key={pageItem.name}>
+								<Link variant={'footer'}>{pageItem.name}</Link>
+							</NextLink>
+						);
+					})}
+				</Stack>
 				<Spacer></Spacer>
-				{linkList.map((linkItem) => {
-					return (
-						<IconButton
-							variant='outline'
-							size={'md'}
-							key={linkItem.name}
-							aria-label={`Go to ${linkItem.name} Page`}
-							onClick={() => {
-								window.open(linkItem.link, '_blank');
-							}}
-							icon={linkItem.icon}></IconButton>
-					);
-				})}
-			</HStack>
+				<Stack align='center' spacing={2} direction={'row'}>
+					{linkList.map((linkItem) => {
+						return (
+							<IconButton
+								variant='outline'
+								size={'sm'}
+								key={linkItem.name}
+								aria-label={`Go to ${linkItem.name} Page`}
+								onClick={() => {
+									window.open(linkItem.link, '_blank');
+								}}
+								icon={linkItem.icon}></IconButton>
+						);
+					})}
+				</Stack>
+			</Stack>
 		</Container>
 	);
 }
